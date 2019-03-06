2a039c0a2fe9e93f4a0a02e5beafa169dfdc05cf
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-06 11:50:28 +0200

    TODO

--- core/src/fi/tuni/tiko/CustomDialog.java
+++ core/src/fi/tuni/tiko/CustomDialog.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
  */
 
 //TODO: Implement some kind of automatic line change on text
+//TODO: Implement localization
 //TODO: Make this better
     public class CustomDialog extends Dialog {
     private static String windowStyle = "dialog";
