b82ad8af14e127a96cbf0cbef89f7a659e318b75
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-06 11:50:00 +0200

    Comments

--- core/src/fi/tuni/tiko/CustomDialog.java
+++ core/src/fi/tuni/tiko/CustomDialog.java
@@ -19,6 +19,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
     private static String windowStyle = "dialog";
     private static Skin skin;
 
+    /**
+     * Constructor that is called when you wish to add an image at the top of the dialog
+     * //TODO: Figure out some smart way to resize it to a good size
+     * @param text Text presented in the dialog
+     * @param title Title of the Dialog
+     * @param image Image to be presented on the top
+     */
     public CustomDialog(String text, String title, Image image) {
         super(title, skin, windowStyle);
         getContentTable().add(image).prefWidth(image.getPrefWidth()/5).prefHeight(image.getPrefHeight()/5);
@@ -26,11 +33,20 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
         buildDialog(text);
     }
 
+    /**
+     * Constructor that is called when you have text and title only
+     * @param text Text presented in the dialog
+     * @param title Title of the dialog
+     */
     public CustomDialog(String text, String title) {
         super(title, skin, windowStyle);
         buildDialog(text);
     }
 
+    /**
+     * Builds the text and button portion of the dialog text
+     * @param text Text of the dialog
+     */
     private void buildDialog(String text) {
         if (Main.debug) this.debug();
         text(text);
@@ -41,11 +57,19 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
         remove();
     }
 
+    /**
+     * Override the hide action to get rid of the hide action
+     */
     @Override
     public void hide() {
         hide(null);
     }
 
+    /**
+     * Sets the static skin for all the dialogs. Called in main. This needs to be ran at the start
+     * of the program for the CustomDialog to work
+     * @param skin the skin we wish to use with the CustomDialogs.
+     */
     public static void setDialogSkin(Skin skin) {
         CustomDialog.skin = skin;
     }
