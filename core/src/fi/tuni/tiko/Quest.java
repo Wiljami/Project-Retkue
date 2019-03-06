f9cb82db4c15ce5b54f70502b76f711d845621f4
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-06 16:09:11 +0200

    Added Quest class

---
+++ core/src/fi/tuni/tiko/Quest.java
@@ -0,0 +1,16 @@
+package fi.tuni.tiko;
+
+/**
+ * Quest class. This should hold information about the different quests in the game.
+ * @author Viljami Pietarila
+ * @version 2019.0306
+ */
+public class Quest {
+    private String title;
+    private String shortDescription;
+    private String longDescription;
+    private int reward;
+    private int time;
+    private int difficulty;
+    //private enum questGiver?
+}
