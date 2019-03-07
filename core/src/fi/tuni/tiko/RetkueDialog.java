0feb9ef92e03adbfefb23af8ccbbc4c6fe00d2fc
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-07 08:02:15 +0200

    Comments

--- core/src/fi/tuni/tiko/RetkueDialog.java
+++ core/src/fi/tuni/tiko/RetkueDialog.java
@@ -5,6 +5,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.I18NBundle;
 
+/**
+ * RetkueDialog is a custom dialog pop up used in the game. It extends dialogs and has some changes
+ * compared to it. The actual PopUps need to extend RetkueDialog in turn
+ *
+ * @author Viljami Pietarila
+ * @version 2019.0307
+ */
 public abstract class RetkueDialog extends Dialog {
     public static Skin skin;
     public static Main game;
