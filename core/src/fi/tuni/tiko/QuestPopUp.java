2c6ff5416b8f9d598cfd2d025e87eb1e21308d71
Wiljami <viljami.pietarila@gmail.com>
2019-03-08 11:55:09 +0200

    Worked on the quest accept dialog

--- core/src/fi/tuni/tiko/QuestPopUp.java
+++ core/src/fi/tuni/tiko/QuestPopUp.java
@@ -1,11 +1,20 @@
 package fi.tuni.tiko;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class QuestPopUp extends RetkueDialog {
     private static String windowStyle = "dialog";
 
     private static String title = "";
+
+    private Button no;
+    private Button yes;
+    private Button ok;
+    private RetkueLabel desc;
 
     private static TavernPopUp tavernPopUp;
 
@@ -16,25 +25,58 @@ public class QuestPopUp extends RetkueDialog {
 
         getTitleLabel().setText(readLine("QUEST_001_TITLE"));
 
-        getContentTable().add(image).prefWidth(image.getPrefWidth() / 5).prefHeight(image.getPrefHeight() / 5);
+        String text = readLine("QUEST_001_TEXT");
+
+        yes = new TextButton(readLine("yes"), getSkin());
+        yes.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                acceptQuest();
+            }
+        });
+
+        no = new TextButton(readLine("no"), getSkin());
+        no.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                remove();
+            }
+        });
+
+        ok = new TextButton(readLine("ok"), getSkin());
+        ok.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                closeMe();
+            }
+        });
+
+        ok.setVisible(false);
+
+        getContentTable().add(image).prefWidth(image.getPrefWidth() / 5).prefHeight(image.getPrefHeight() / 5).colspan(3);
         getContentTable().row();
 
-        String text = readLine("QUEST_001_TEXT");
-        String accept = readLine("QUEST_001_ACCEPT");
-        RetkueLabel desc = new RetkueLabel(text);
-        getContentTable().add(desc).prefWidth(250);
 
-        button(readLine("yes"), true); //sends "true" as the result
-        button(readLine("no"), false);  //sends "false" as the result
+        desc = new RetkueLabel(text);
+        getContentTable().add(desc).prefWidth(250).colspan(3);
+
+        getContentTable().row();
+
+        getContentTable().add(yes).pad(10);
+        getContentTable().add(ok).pad(10);
+        getContentTable().add(no).pad(10);
     }
 
+    public void closeMe() {
+        tavernPopUp.remove();
+        remove();
+    }
 
-
-    public void result(Object obj) {
-        if (obj.equals(true)) {
-            System.out.println("ok");
-            tavernPopUp.remove();
-            button(readLine("ok"), true);
-        }
+    public void acceptQuest() {
+        String accept = readLine("QUEST_001_ACCEPT");
+        desc.setText(accept);
+        no.setVisible(false);
+        yes.setVisible(false);
+        ok.setVisible(true);
     }
 }
