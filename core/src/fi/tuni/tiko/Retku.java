0b58451933c6f4db6fb5624aaec71739aebed4ca
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-10 19:04:41 +0200

    Comments

--- core/src/fi/tuni/tiko/Retku.java
+++ core/src/fi/tuni/tiko/Retku.java
@@ -1,13 +1,39 @@
 package fi.tuni.tiko;
 
+/**
+ * Retku class holds information and functionality of a Retku.
+ *
+ * A retku is a character element in the game. A party is formed of three Retkus.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.0310
+ */
 public class Retku {
+    /**
+     * Name of the Retku
+     */
     private String name;
 
+    /**
+     * Max health of Retku
+     */
     private int maxHealth;
+
+    /**
+     * Current Health of Retku
+     */
     private int currHealth;
 
+    /**
+     * Image of the Retku
+     */
     private String imageFile;
 
+    /**
+     * Retku constructor
+     * @param name name of the Retku
+     * @param health max Health of Retku
+     */
     public Retku(String name, int health) {
         setMaxHealth(health);
         setCurrHealth(health);
@@ -15,40 +41,76 @@ public class Retku {
         imageFile = "old_guy1.png";
     }
 
+    /**
+     * get MaxHealth
+     * @return maxHealth
+     */
     public int getMaxHealth() {
         return maxHealth;
     }
 
+    /**
+     * set maxHealth
+     * @param maxHealth new maxHealth
+     */
     public void setMaxHealth(int maxHealth) {
         this.maxHealth = maxHealth;
     }
 
+    /**
+     * get currHealth
+     * @return currHealth
+     */
     public int getCurrHealth() {
         return currHealth;
     }
 
+    /**
+     * set currHealth
+     * @param currHealth currHealth
+     */
     public void setCurrHealth(int currHealth) {
         if (currHealth > maxHealth) currHealth = maxHealth;
         this.currHealth = currHealth;
     }
 
+    /**
+     * get name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * set name
+     * @param name name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * get healthPercentage of the Retku
+     * @return float between 0f and 1.0f as percentage
+     */
     public float healthPercentage() {
         float healthPercentage = (float)getCurrHealth() / (float)getMaxHealth();
         return healthPercentage;
     }
 
+    /**
+     * get imageFile
+     * @return imageFile
+     */
     public String getImageFile() {
         return imageFile;
     }
 
+    /**
+     * damageRetku reduces the Retku's currHealth by the damage amount
+     * @param damage damage done
+     */
     public void damageRetku(int damage) {
         setCurrHealth(getCurrHealth() - damage);
         if (currHealth <= 0) {
@@ -56,6 +118,10 @@ public class Retku {
         }
     }
 
+    /**
+     * healRetku increases the Retku's currHealth by the heal amount
+     * @param heal heal amount
+     */
     public void healRetku(int heal) {
         setCurrHealth(getCurrHealth() + heal);
     }
