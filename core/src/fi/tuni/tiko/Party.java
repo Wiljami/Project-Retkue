7efc079826d54cff25c9b1d9cf3b489ed6fce6e6
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-10 18:36:09 +0200

    Comments

--- core/src/fi/tuni/tiko/Party.java
+++ core/src/fi/tuni/tiko/Party.java
@@ -8,16 +8,35 @@ package fi.tuni.tiko;
  * @version 2019.0310
  */
 public class Party {
+    /**
+     * Array of the Retkus in the party
+     */
     private Retku[] retkus;
 
+    /**
+     * Player gold
+     */
     private int gold;
+    /**
+     * Player steps
+     */
     private int steps;
 
     //How many steps for gold piece
     //TODO: These values need some thinking through
+    /**
+     * The amount of steps per conversion
+     */
     private static final int STEPSTOGOLD = 100;
+    /**
+     * The amount of gold gained per conversion
+     */
     private static final int GOLDFROMSTEPS = 5;
 
+    /**
+     * Party constructor. atm has dummy values for the retkus and gold and steps
+     * TODO: Change this.
+     */
     public Party() {
         retkus = new Retku[3];
         retkus[0] = new Retku("Bill", 100);
@@ -27,27 +46,52 @@ public class Party {
         gold = 99;
     }
 
+    /**
+     * Find and return the Retku at spot n
+     * @param n the spot we wish to check
+     * @return Retku of that position
+     */
     public Retku findRetku(int n) {
         return retkus[n];
     }
 
+    /**
+     * Player gold
+     * @return gold
+     */
     public int getGold() {
         return gold;
     }
 
+    /**
+     * Player steps
+     * @return steps
+     */
     public int getSteps() {
         return steps;
     }
 
+    /**
+     * Set gold. Does not allow negative gold.
+     * @param gold new gold value
+     */
     public void setGold(int gold) {
         this.gold = gold;
         if(this.gold < 0) this.gold = 0;
     }
 
+    /**
+     * Spend n amount of gold
+     * @param n gold amount
+     */
     public void spendGold(int n) {
         setGold(getGold() - n);
     }
 
+    /**
+     * Convert steps to gold. If the conversion is not possible, then return false.
+     * @return boolean wether the conversion was possible.
+     */
     public boolean convert() {
         if (steps < STEPSTOGOLD) {
             return false;
