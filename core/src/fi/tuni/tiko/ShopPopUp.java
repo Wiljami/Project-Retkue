fa981a1abd009cca0f3c401a8e5e666780b23b95
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-10 19:09:23 +0200

    Comments

--- core/src/fi/tuni/tiko/ShopPopUp.java
+++ core/src/fi/tuni/tiko/ShopPopUp.java
@@ -6,17 +6,35 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+/**
+ * ShopPopUp class holds the functionality and the UI elements of the game's shop.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.0310
+ */
 public class ShopPopUp extends RetkueDialog {
+    /**
+     * windowStyle of the shop
+     */
     private static String windowStyle = "dialog";
 
+    /**
+     * Title name of the shop window
+     */
     private static String title = readLine("shop");
 
+    /**
+     * ShopPopUp constructor
+     */
     public ShopPopUp() {
         super(title, skin, windowStyle);
         createMenu();
         if (Main.debug) debug();
     }
 
+    /**
+     * createMenu creates various UI actors
+     */
     private void createMenu() {
         String text = readLine("shop_desc");
         RetkueLabel desc = new RetkueLabel(text);
