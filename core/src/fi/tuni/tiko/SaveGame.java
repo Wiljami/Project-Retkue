821695391a7f77e529eba34f25a34a5dc34bf263
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-13 11:55:23 +0200

    Working on save and load

--- core/src/fi/tuni/tiko/SaveGame.java
+++ core/src/fi/tuni/tiko/SaveGame.java
@@ -7,11 +7,22 @@ public class SaveGame {
     public static void save(String saveFile, Party party) {
         Preferences save = (Gdx.app.getPreferences(saveFile));
         save.putString("title", "Retkue Save");
+
+        saveConfig(save);
+        saveParty(save, party);
+
+        save.flush();
+    }
+
+    private static void saveConfig (Preferences save) {
         save.putString("language",  Config.getLanguageName());
         save.putBoolean("audio", Config.isAudio());
         save.putFloat("stepStartPosition", Config.getStepStartPosition());
+    }
 
-        save.flush();
+    private static void saveParty (Preferences save, Party party) {
+        save.putInteger("gold", party.getGold());
+        save.putInteger("steps", party.getSteps());
     }
 
     public static boolean load(String saveFile, Party party) {
@@ -20,22 +31,37 @@ public class SaveGame {
         if (title.equals("null")) {
             return false;
         }
+        System.out.println(title);
 
+        loadConfig(save);
+        loadParty(save, party);
+
+        return true;
+    }
+
+    private static void loadConfig(Preferences save) {
         String language = save.getString("language", "null");
         setLanguage(language);
 
-        Float steps = save.getFloat("stepStartPosition", 0.0f);
-        Config.setStepStartPosition(steps);
+        Float stepStart = save.getFloat("stepStartPosition", 0.0f);
+        Config.setStepStartPosition(stepStart);
 
         Boolean audio = save.getBoolean("audio", false);
         Config.setAudio(audio);
 
-        System.out.println(title);
         System.out.println(language);
-        System.out.println(steps);
+        System.out.println(stepStart);
         System.out.println(audio);
+    }
 
-        return true;
+    private static void loadParty(Preferences save, Party party) {
+        int gold = save.getInteger("gold", 0);
+        party.setGold(gold);
+        int steps = save.getInteger("gold", 0);
+        party.setSteps(steps);
+
+        System.out.println(gold);
+        System.out.println(steps);
     }
 
     private static void setLanguage(String language) {
