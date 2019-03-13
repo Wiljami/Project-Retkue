027af639c38138a8c9ee3d2e041eb77f95de9b67
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-03-13 09:13:30 +0200

    mute music

--- core/src/fi/tuni/tiko/Main.java
+++ core/src/fi/tuni/tiko/Main.java
@@ -74,7 +74,7 @@ public class Main extends Game {
 		batch = new SpriteBatch();
 		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("retkuetheme.ogg"));
         backgroundMusic.setLooping(true);
-        backgroundMusic.play();
+        //backgroundMusic.play();
         //TODO: Create the load and save. Here we need to check if a party already exists and load it.
         party = new Party();
 		initiateScenes();
