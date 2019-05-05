dcf33dc55cd9ad9e595152e1b47cd07258067923
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-05-05 21:44:30 +0300

    Comments

--- core/src/fi/tuni/retkue/AnimatedActor.java
+++ core/src/fi/tuni/retkue/AnimatedActor.java
@@ -6,18 +6,72 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+/**
+ * AnimatedActor is Actor that is animated and holds two different states
+ *
+ * @author Viljami Pietarila
+ * @version 2019.0505
+ */
 public class AnimatedActor extends Actor {
+    /**
+     * Animation animation is the current animation
+     */
     Animation animation;
+
+    /**
+     * Animation woundedAnimation is the wounded animation
+     */
     Animation woundedAnimation;
+
+    /**
+     * Animation normalAnimation is the normal animation
+     */
     Animation normalAnimation;
+
+    /**
+     * float stateTime is the state of the animation
+     */
     private float stateTime;
+
+    /**
+     * float speed is the speed of the animation
+     */
     private float speed;
+
+    /**
+     * float width is the width of the animation
+     */
     private float width;
+
+    /**
+     * float height is the height of the animation
+     */
     private float height;
+
+    /**
+     * int cols is the cols of the animation sheet
+     */
     private int cols;
+
+    /**
+     * int rows is the rows of the animation sheet
+     */
     private int rows;
+
+    /**
+     * TextureRegion currentFrame is the current displayed animation frame
+     */
     TextureRegion currentFrame;
 
+    /**
+     * Constructor for AnimatedActor
+     * @param file filename of the animation sheet
+     * @param cols columns in animation sheet
+     * @param rows rows in animation sheet
+     * @param speed speed of the animation
+     * @param width width of the animation
+     * @param height height of the animation
+     */
     public AnimatedActor(String file, int cols, int rows, float speed, float width, float height) {
         this.width = width;
         this.height = height;
@@ -34,6 +88,11 @@ public class AnimatedActor extends Actor {
         animation = normalAnimation;
     }
 
+    /**
+     * initiateWoundedAnimation generates the woundedAnimation. It uses same parameters as the
+     * normal animnation
+     * @param file filename of the woundedanimation sheet
+     */
     public void initateWoundedAnimation(String file) {
         Texture animationSheet = Utils.loadTexture(file);
         TextureRegion[][] tmp = TextureRegion.split(
@@ -44,6 +103,11 @@ public class AnimatedActor extends Actor {
         woundedAnimation = new Animation(speed, frames);
     }
 
+    /**
+     * Override for draw to draw our animation
+     * @param batch spriteBatch
+     * @param parentAlpha parentAlpha
+     */
     @Override
     public void draw(Batch batch, float parentAlpha) {
         currentFrame = (TextureRegion)animation.getKeyFrame(stateTime, true);
@@ -52,16 +116,26 @@ public class AnimatedActor extends Actor {
         batch.draw(currentFrame, x, y,  width, height);
     }
 
+    /**
+     * Override for act to run our stateTime
+     * @param delta deltaTime
+     */
     @Override
     public void act(float delta) {
         super.act(delta);
         stateTime += delta;
     }
 
+    /**
+     * Sets the animation to woundedAnimation
+     */
     public void setWoundedAnimation() {
         animation = woundedAnimation;
     }
 
+    /**
+     * Sets the animation to normalAnimation
+     */
     public void setNormalAnimation() {
         animation = normalAnimation;
     }
